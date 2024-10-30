6947495cb2b733e43a499166c5a68af1e84f67a8
JP Appel <jeanpierre.appel01@gmail.com>
2024-10-30 00:26:55 -0400

    Add game serializer and tests

---
+++ tests/test_serialization.py
@@ -0,0 +1,23 @@
+import json
+
+import game
+import serialization
+
+
+def test_serialize_board():
+    board = game._example_game(3)
+    serialized = json.dumps(board, cls=serialization.BoardEncoder)
+    deserialized = json.loads(serialized)
+    assert board.id == deserialized["id"]
+    assert board.seed == deserialized["seed"]
+    assert board.size == deserialized["size"]
+
+    tiles = [tile for row in board.board for tile in row]
+    for board_tile, deserialized_tile in zip(tiles, deserialized["tiles"], strict=True):
+        type = "text" if board_tile.image_url is None else "image"
+        content = board_tile.image_url or board_tile.text
+        assert type == deserialized_tile["type"]
+        assert content == deserialized_tile["content"]
+
+        if len(board_tile.tags) != 0:
+            assert board_tile.tags == frozenset(deserialized_tile["tags"])
