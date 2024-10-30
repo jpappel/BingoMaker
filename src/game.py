cc81f52d7d4aa330d35856694137c14ef787e681
JP Appel <jeanpierre.appel01@gmail.com>
2024-10-29 23:01:29 -0400

    Move randomness seed from tile pool to board

--- src/game.py
+++ src/game.py
@@ -38,11 +38,9 @@ class TilePool:
         self,
         tiles: frozenset[Tile],
         free_square: Tile | None = None,
-        seed: None | int = None,
     ):
         self.tiles = tiles
         self.free = free_square
-        self.seed = seed
 
     def __len__(self):
         return len(self.tiles)
@@ -62,13 +60,15 @@ class TilePool:
 
         return self.free
 
-    def get_tile(self, exclude_tags: list[str] | None = None) -> Tile:
+    def get_tile(
+        self, exclude_tags: list[str] | None = None, seed: None | int = None
+    ) -> Tile:
         """Get a tile from the tile pool"""
         tiles = (
             self.tiles if exclude_tags is None else self._filter_by_tags(exclude_tags)
         )
         try:
-            random.seed(self.seed)
+            random.seed(seed)
             tile = random.choice(tuple(tiles))
         except IndexError as e:
             raise NoMatchingTile("No more valid tiles in pool") from e
@@ -77,14 +77,29 @@ class TilePool:
 
 
 class Board:
-    def __init__(self, pool: TilePool, size: int = 5, free_square: bool = True):
+    def __init__(
+        self,
+        pool: TilePool,
+        size: int = 5,
+        free_square: bool = True,
+        seed: int = 0,
+    ):
         self.board = [[Tile("")] * size] * size
         self.checked = [[False] * size] * size
+        self.size = size
+        self.seed = seed
+        self.id = ""
         for x in range(size):
             for y in range(size):
-                self.board[x][y] = pool.get_tile()
+                self.board[x][y] = pool.get_tile(seed=self.seed * x + y)
 
         if free_square:
             mid = size // 2
             self.board[mid][mid] = pool.get_free()
             self.checked[mid][mid] = True
+
+
+def _example_game(size: int) -> Board:
+    tiles = frozenset([Tile(f"Tile {i}") for i in range((size + 1) ** 2)])
+    pool = TilePool(tiles)
+    return Board(pool, free_square=False, seed=0)
