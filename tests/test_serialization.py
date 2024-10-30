43a34b6104feae0dee36ec1af95bde6f3ac0c5e1
JP Appel <jeanpierre.appel01@gmail.com>
2024-10-30 00:48:51 -0400

    Update serialization test to test more cases

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -5,19 +5,25 @@ import serialization
 
 
 def test_serialize_board():
-    board = game._example_game(3)
-    serialized = json.dumps(board, cls=serialization.BoardEncoder)
-    deserialized = json.loads(serialized)
-    assert board.id == deserialized["id"]
-    assert board.seed == deserialized["seed"]
-    assert board.size == deserialized["size"]
+    def test_board(size: int):
+        board = game._example_game(size)
+        serialized = json.dumps(board, cls=serialization.BoardEncoder)
+        deserialized = json.loads(serialized)
+        assert board.id == deserialized["id"]
+        assert board.seed == deserialized["seed"]
+        assert board.size == deserialized["size"]
 
-    tiles = [tile for row in board.board for tile in row]
-    for board_tile, deserialized_tile in zip(tiles, deserialized["tiles"], strict=True):
-        type = "text" if board_tile.image_url is None else "image"
-        content = board_tile.image_url or board_tile.text
-        assert type == deserialized_tile["type"]
-        assert content == deserialized_tile["content"]
+        tiles = [tile for row in board.board for tile in row]
+        for board_tile, deserialized_tile in zip(
+            tiles, deserialized["tiles"], strict=True
+        ):
+            type = "text" if board_tile.image_url is None else "image"
+            content = board_tile.image_url or board_tile.text
+            assert type == deserialized_tile["type"]
+            assert content == deserialized_tile["content"]
 
-        if len(board_tile.tags) != 0:
-            assert board_tile.tags == frozenset(deserialized_tile["tags"])
+            if len(board_tile.tags) != 0:
+                assert board_tile.tags == frozenset(deserialized_tile["tags"])
+
+    for i in range(1, 7):
+        test_board(i)
