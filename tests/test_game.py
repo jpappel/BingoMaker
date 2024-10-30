3b5908fb48c9ba4360d5f68bf7ab606bd85962a7
JP Appel <jeanpierre.appel01@gmail.com>
2024-10-29 23:01:29 -0400

    Move randomness seed from tile pool to board

--- tests/test_game.py
+++ tests/test_game.py
@@ -6,9 +6,9 @@ def test_consistent_get_tile():
     length = 30
 
     tiles = frozenset([Tile(f"{x}") for x in range(length)])
-    pool = TilePool(tiles, None, seed)
+    pool = TilePool(tiles, None)
     tiles_2 = frozenset([Tile(f"{x}") for x in range(length)])
-    pool_2 = TilePool(tiles_2, None, seed)
+    pool_2 = TilePool(tiles_2, None)
 
     for _ in range(length):
-        assert pool.get_tile() == pool_2.get_tile()
+        assert pool.get_tile(seed=seed) == pool_2.get_tile(seed=seed)
