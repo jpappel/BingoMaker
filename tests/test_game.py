47bc01b5402aac84a8550e45d02fb1bddfd114ab
JP Appel <jeanpierre.appel01@gmail.com>
2024-11-02 11:29:10 -0400

    Add test for free tile placement

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,4 +1,17 @@
-from src.game.game import Tile, TilePool
+from src.game.game import Board, Tile, TilePool
+
+
+def _example_game(size: int, seed: int = 0, free_square: bool = False) -> Board:
+    assert size > 0
+    tiles = frozenset(
+        [
+            Tile(f"Tile {i}", tags=frozenset([str(i), f"tag_{i % size}"]))
+            for i in range((size + 1) ** 2)
+        ]
+    )
+    free_tile = Tile("Free", frozenset(), "free_url") if free_square else None
+    pool = TilePool(tiles, free_square=free_tile)
+    return Board(pool, size=size, free_square=free_square, seed=seed)
 
 
 def test_consistent_get_tile():
@@ -12,3 +25,17 @@ def test_consistent_get_tile():
 
     for _ in range(length):
         assert pool.get_tile(seed=seed) == pool_2.get_tile(seed=seed)
+
+
+def test_set_free_tile():
+    # test an odd size board
+    game = _example_game(5, 0, True)
+    middle_tile = game.board[2][2]
+    assert middle_tile.text == "Free"
+    assert middle_tile.image_url == "free_url"
+
+    # test an even size board
+    game = _example_game(6, 0, True)
+    middle_tile = game.board[3][3]
+    assert middle_tile.text == "Free"
+    assert middle_tile.image_url == "free_url"
