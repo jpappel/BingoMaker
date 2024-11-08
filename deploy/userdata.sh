bae099892fab6921fa8a19a446167c84fdcb6849
Owen Halliday <owen@owenhalliday.net>
2024-11-08 00:38:46 -0500

    pin uv version and force install directory

--- deploy/userdata.sh
+++ deploy/userdata.sh
@@ -6,6 +6,7 @@ export HOME=/root
 git clone https://github.com/cs399f24/BingoMaker
 cd BingoMaker
 
-curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
+curl -LsSf https://astral.sh/uv/0.5.0/install.sh | env UV_UNMANAGED_INSTALL=/usr/bin sh
+# source $HOME/.local/bin/env
+uv sync
 make deploy
