d5633b89bdcab18712fd1ee773d80eaae7b8425c
Owen Halliday <hallidayo@moravian.edu>
2024-11-07 20:40:56 -0500

    fix syntax errors

--- deploy/ec2.sh
+++ deploy/ec2.sh
@@ -3,6 +3,6 @@ aws ec2 run-instances --image-id "ami-06b21ccaeff8cd686" --instance-type "t2.mic
     --credit-specification '{"CpuCredits":"standard"}' \
     --tag-specifications '{"ResourceType":"instance","Tags":[{"Key":"Name","Value":"bingo-maker"}]}' \
     --metadata-options '{"HttpEndpoint":"enabled","HttpPutResponseHopLimit":2,"HttpTokens":"required"}' \
-    -private-dns-name-options '{"HostnameType":"ip-name","EnableResourceNameDnsARecord":true,"EnableResourceNameDnsAAAARecord":false}' \
+    --private-dns-name-options '{"HostnameType":"ip-name","EnableResourceNameDnsARecord":true,"EnableResourceNameDnsAAAARecord":false}' \
     --user-data file://deploy/userdata.sh \
     --count "1" 
--- deploy/security-group.sh
+++ deploy/security-group.sh
@@ -1,6 +1,6 @@
 aws ec2 create-security-group --group-name "bingo-maker-ec2" --description "Allow http(s), ssh, and database access" \
     --vpc-id "vpc-021dbf31759b3a397" \
-    --tag-specifications '{"resourceType":"security-group","tags":[{"key":"purpose","value":"bingo-maker"},{"key":"lifespan","value":"indeterminate"}]}' 
+    --tag-specifications '{"ResourceType":"security-group","Tags":[{"Key":"purpose","Value":"bingo-maker"},{"Key":"lifespan","Value":"indeterminate"}]}' 
 
 aws ec2 authorize-security-group-ingress --group-id "sg-06a8b744eb03a0dd2" \
     --ip-permissions '{"FromPort":22,"ToPort":22,"IpProtocol":"tcp","IpRanges":[{"CidrIp":"0.0.0.0/0"}]}' '{"FromPort":80,"ToPort":80,"IpProtocol":"tcp","IpRanges":[{"CidrIp":"0.0.0.0/0"}]}' '{"FromPort":443,"ToPort":443,"IpProtocol":"tcp","IpRanges":[{"CidrIp":"0.0.0.0/0"}]}' 
