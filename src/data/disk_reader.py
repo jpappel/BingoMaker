828c1a33f153ac2392e6d04a0ad3a0b1f109fd95
JP Appel <jeanpierre.appel01@gmail.com>
2024-11-07 23:19:47 -0500

    Add disk based storage

--- src/data/disk_reader.py
+++ src/data/disk_reader.py
@@ -1,4 +1,10 @@
+import json
+import os
+import uuid
+from pathlib import Path
+
 from game.game import Tile, TilePool
+from src.data.persistence import DBResult, TilePoolDB, tile_to_dict, dict_to_tile
 
 
 def read_text(path: str) -> TilePool:
@@ -8,3 +14,108 @@ def read_text(path: str) -> TilePool:
             tiles.append(Tile(line.strip()))
 
     return TilePool(frozenset(tiles))
+
+
+class DiskTilePoolDB(TilePoolDB):
+    def __init__(self, root_dir: str | Path):
+        self.root = root_dir if isinstance(root_dir, Path) else Path(root_dir)
+
+        if self.root.exists() and not self.root.is_dir():
+            raise NotADirectoryError("path exists but is not a directory")
+
+        if not self.root.exists():
+            self.root.mkdir()
+
+    def _find_first_by_id(self, tile_pool_id: str) -> Path | None:
+        filename = f"{tile_pool_id}.json"
+        for dirpath, _, filenames in self.root.walk():
+            if filename in filenames:
+                return dirpath / filename
+
+    def insert_tile_pool(self, name: str, owner: str, pool: TilePool) -> str | None:
+        dir_ = self.root / owner
+        dir_.mkdir(exist_ok=True)
+
+        id_ = uuid.uuid1().hex + ".json"
+        with open(dir_ / id_, "w") as f:
+            to_write = {
+                "Owner": owner,
+                "Name": name,
+                "Tiles": [tile_to_dict(tile) for tile in pool.tiles],
+                "CreatedAt": 0,
+            }
+            if pool.free:
+                to_write["FreeTile"] = tile_to_dict(pool.free)
+            json.dump(to_write, f)
+
+    def delete_tile_pool(self, tile_pool_id: str) -> bool:
+        try:
+            filepath = self._find_first_by_id(tile_pool_id)
+            if filepath:
+                os.remove(filepath)
+                return True
+        except Exception as e:
+            print(e)
+
+        return False
+
+    def delete_tile_pool_by_owner(self, owner: str) -> bool:
+        dir_ = self.root / owner
+        try:
+            for _, _, filenames in dir_.walk():
+                for filename in filenames:
+                    filepath = dir_ / filename
+                    os.remove(filepath)
+            dir_.rmdir()
+            return True
+        except Exception as e:
+            print(e)
+        return False
+
+    def update_tiles(
+        self,
+        tile_pool_id: str,
+        removals: list[str] | None = None,
+        insertions: list[Tile] | None = None,
+    ) -> bool:
+        if removals is None and insertions is None:
+            return False
+
+        try:
+            result = self.get_tile_pool(tile_pool_id)
+            if result is None:
+                return False
+            pool = result["tiles"]
+            raise NotImplementedError()
+
+        except Exception as e:
+            print(e)
+        return False
+
+    def get_tile_pools(self, quantity: int | None = None) -> list[DBResult] | None:
+        pass
+
+    def get_tile_pool(self, tile_pool_id: str) -> DBResult | None:
+        try:
+            filepath = self._find_first_by_id(tile_pool_id)
+            if filepath is None:
+                return
+
+            with open(filepath) as file:
+                unparsed = json.load(file)
+                tiles = frozenset(dict_to_tile(item) for item in unparsed["Tiles"])
+                free = (
+                    dict_to_tile(unparsed["FreeTile"])
+                    if "FreeTile" in unparsed
+                    else None
+                )
+                pool = TilePool(tiles, free)
+                return {
+                    "id": tile_pool_id,
+                    "owner": unparsed["Owner"],
+                    "name": unparsed["Name"],
+                    "created_at": unparsed["CreatedAt"],
+                    "tiles": pool,
+                }
+        except Exception as e:
+            print(e)
---
+++ src/data/persistence.py
@@ -0,0 +1,68 @@
+import abc
+from typing import TypedDict
+
+from game.game import Tile, TilePool
+
+
+class DBResult(TypedDict):
+    owner: str
+    name: str
+    tiles: TilePool
+    id: str
+    created_at: str
+
+
+class TileDict(TypedDict):
+    Content: str
+    Type: str
+    Tags: list[str]
+
+
+def tile_to_dict(tile: Tile) -> TileDict:
+    return {
+        "Content": tile.text if tile.image_url is None else tile.image_url,
+        "Type": "text" if tile.image_url is not None else "image",
+        "Tags": list(tile.tags),
+    }
+
+
+def dict_to_tile(item: dict[str, str | list[str]]) -> Tile:
+    if not isinstance(item["Content"], str):
+        raise Exception()
+    text = item["Content"]
+    tags = frozenset(item["Tags"])
+    image_url = item["Content"] if item["Type"] == "image" else None
+    return Tile(text, tags, image_url)
+
+
+class TilePoolDB(abc.ABC):
+    """Abstract class of that can create, modify, delete TilePools from storage"""
+
+    @abc.abstractmethod
+    def insert_tile_pool(self, name: str, owner: str, pool: TilePool) -> str | None:
+        pass
+
+    @abc.abstractmethod
+    def delete_tile_pool(self, tile_pool_id: str) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def delete_tile_pool_by_owner(self, owner: str) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def update_tiles(
+        self,
+        tile_pool_id: str,
+        removals: list[str] | None = None,
+        insertions: list[Tile] | None = None,
+    ) -> bool:
+        pass
+
+    @abc.abstractmethod
+    def get_tile_pools(self, quantity: int | None = None) -> list[DBResult] | None:
+        pass
+
+    @abc.abstractmethod
+    def get_tile_pool(self, tile_pool_id: str) -> DBResult | None:
+        pass
