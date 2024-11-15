7c4692354188eb7373f82ec949f46ee0987c71b8
JP Appel <jeanpierre.appel01@gmail.com>
2024-11-15 00:50:46 -0500

    Add tests for ImageManagers

--- src/images/__init__.py
+++ src/images/__init__.py
@@ -1,4 +1,11 @@
 from .image_manager import ImageManager, ReferenceCounts
 from .local import LocalImageManager, LocalReferenceCounts
+from .memory import MemoryReferenceCounts
 
-__all__ = ["ImageManager", "ReferenceCounts", "LocalImageManager", "LocalReferenceCounts"]
+__all__ = [
+    "ImageManager",
+    "ReferenceCounts",
+    "LocalImageManager",
+    "LocalReferenceCounts",
+    "MemoryReferenceCounts",
+]
