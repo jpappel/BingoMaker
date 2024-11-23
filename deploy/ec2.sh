208ca8b6e627dfac98f320f78f41bee10a336e3d
Owen Halliday <hallidayo@moravian.edu>
2024-11-23 16:40:00 -0500

    delete original scripts

--- deploy/ec2.sh
+++
@@ -1,8 +0,0 @@
-aws ec2 run-instances --image-id "ami-06b21ccaeff8cd686" --instance-type "t2.micro" \
-    --key-name "vockey" --network-interfaces '{"AssociatePublicIpAddress":true,"DeviceIndex":0,"Groups":["sg-06a8b744eb03a0dd2"]}' \
-    --credit-specification '{"CpuCredits":"standard"}' \
-    --tag-specifications '{"ResourceType":"instance","Tags":[{"Key":"Name","Value":"bingo-maker"}]}' \
-    --metadata-options '{"HttpEndpoint":"enabled","HttpPutResponseHopLimit":2,"HttpTokens":"required"}' \
-    --private-dns-name-options '{"HostnameType":"ip-name","EnableResourceNameDnsARecord":true,"EnableResourceNameDnsAAAARecord":false}' \
-    --user-data file://deploy/userdata.sh \
-    --count "1" 
