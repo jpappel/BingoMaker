aee54753e841fe7452ffb35effd38cd79bf0c982
Owen Halliday <hallidayo@moravian.edu>
2024-12-04 15:55:05 -0500

    formatting

--- deploy/lambda.tf
+++ deploy/lambda.tf
@@ -13,15 +13,15 @@ data "aws_iam_role" "lab_role" {
 resource "aws_lambda_function" "functions" {
   for_each = data.local_file.lambda_files
 
-  function_name               = "${replace(each.key, ".py", "")}"
-  handler                     = "lambda_handler"
-  runtime                     = "python3.13"
-  timeout                     = 5
+  function_name = replace(each.key, ".py", "")
+  handler       = "lambda_handler"
+  runtime       = "python3.13"
+  timeout       = 5
 
-  filename                    = "../lambdas/${each.key}.zip" 
-  source_code_hash            = data.archive_file.function_zip[each.key].output_base64sha256
+  filename         = "../lambdas/${each.key}.zip"
+  source_code_hash = data.archive_file.function_zip[each.key].output_base64sha256
 
-  role                        = data.aws_iam_role.lab_role.arn
+  role = data.aws_iam_role.lab_role.arn
 
   layers = [
     aws_lambda_layer_version.base_layer.arn,
@@ -31,26 +31,26 @@ resource "aws_lambda_function" "functions" {
 
 
 resource "aws_lambda_layer_version" "helper_layer" {
-    filename = "../layers/helper_layer/helper_layer.zip"
-    layer_name = "helper_layer"
-    compatible_runtimes = ["python3.13"]
+  filename            = "../layers/helper_layer/helper_layer.zip"
+  layer_name          = "helper_layer"
+  compatible_runtimes = ["python3.13"]
 }
 
 resource "aws_lambda_layer_version" "base_layer" {
-    filename = "../layers/base_layer/base_layer.zip"
-    layer_name = "base_layer"
-    compatible_runtimes = ["python3.13"]
+  filename            = "../layers/base_layer/base_layer.zip"
+  layer_name          = "base_layer"
+  compatible_runtimes = ["python3.13"]
 }
 
 # Archive each Python file into a zip
 data "archive_file" "function_zip" {
   for_each = data.local_file.lambda_files
 
-  source_file  = "../lambdas/${each.key}"  # Reference the function source dir
+  source_file = "../lambdas/${each.key}" # Reference the function source dir
   type        = "zip"
   output_path = "../lambdas/${each.key}.zip" # Zipping each function folder
 }
 
 output "lambda_files" {
-    value = data.local_file.lambda_files
+  value = data.local_file.lambda_files
 }
